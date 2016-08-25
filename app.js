9a4d25881806e541c35743fd46d6234395406d46
sammia28 <sammia.zubizarreta@gmail.com>
2016-08-25 14:12:06 -0500

    ejercicio2

---
+++ app.js
@@ -0,0 +1,5 @@
+var num1 = prompt("Primer número");
+var num2 = prompt("Segundo número");
+var num3 = prompt("Tercer número")
+var suma = (parseInt(num1) + parseInt(num2) +parseInt(num3))/3;
+    window.alert (suma);
